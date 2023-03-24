fdf08cb3f093372b4f0477c4cb5dde6f46f0774d
binarydata123 <binarydata.sale@gmail.com>
2023-03-24 13:47:51 +0530

    updated 2 pages 24march23

---
+++ pages/chefs.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Chefs.module.css'
+import React from 'react';
+import FrontendChefs from '../components/frontend/Chefs';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Chefs() {
+  return (
+    <>
+      <FrontendChefs/>
+    </>
+  )
+}
