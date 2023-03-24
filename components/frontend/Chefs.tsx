f11253a7ec40d0267c7ced9a321118bb0054c26a
binarydata123 <binarydata.sale@gmail.com>
2023-03-24 18:52:22 +0530

    new update pages

--- components/frontend/Chefs.tsx
+++ components/frontend/Chefs.tsx
@@ -10,7 +10,7 @@ export default function Chefs() {
                     </div>
                     <div className="col-sm-6">
                     <div className="banner-text pages-text  margin-sp">
-                    <img src="images/chef.png" alt="chef" />
+                    <img src="images/chef.png" alt="chef" className="chef-img" />
                     <h1>Athens, Greece</h1>
                         <p className="star-review">
                         <i className="fa-solid fa-star"></i>
@@ -28,96 +28,113 @@ export default function Chefs() {
                 </div>
             </section>
 
-            <section className="services-part location-how mt-5">
+            <section className="services-part mt-5">
                 <div className="container">
-                    <h2>How it works</h2> 
-                    <p className="dis-max-width mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. </p>
+                    <p className="font-sb-title-up"> Get to know me better </p>
+                    <h2><i>“Perfection and extraordinery taste is my goal <br/> in every dish”</i></h2> 
+                    <p className="dis-max-width mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. </p> 
+                </div>
+            </section>
+
+            <section className="services-part location-how  mt-5 tab-m-0 tab-p-0">
+                <div className="container"> 
+                    <h2>My services</h2> 
+                    <p className="dis-max-width mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. Nascetur sapien sollicitudin eu consequat. Sem sed accumsan aliquet dapibus tincidunt lobortis sed mauris.</p>
                     <div className="row mt-5">
                     <div className="col-lg-3 col-md-6">
                          <div className="step-box">
                           <h4><span className="big-48">1</span></h4>
-                          <h4>Step 1</h4>
+                          <h4>Personal Chef</h4>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. </p>
                          </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
                          <div className="step-box">
                            <h4><span className="big-48">2</span></h4>
-                           <h4>Step 2</h4>
+                           <h4>Full Time Chef</h4>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. </p>
                          </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
                          <div className="step-box">
                            <h4><span className="big-48">3</span></h4>
-                           <h4>Step 3</h4>
+                           <h4>Cooking Classes</h4>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. </p>
                          </div>
                         </div>
                         <div className="col-lg-3 col-md-6">
                          <div className="step-box">
                          <h4><span className="big-48">4</span></h4>
-                          <h4>Step 4</h4>
+                          <h4>Cooking Show</h4>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. </p>
                          </div>
                         </div>
-                    </div>
- 
-                    
+                    </div> 
                 </div>
             </section>
- 
-            <section className="services-part location-how mt-5 mobile-m-0">
-                <div className="container">
-                    <h2>Chefs for this location</h2> 
-                    <p className="dis-max-width mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. Nascetur sapien sollicitudin eu consequat. Sem sed accumsan aliquet dapibus tincidunt lobortis sed mauris.</p>
+
+            <section className="messages-part">
+                <div className="container ">
+                 <div className="messages-text mt-5 tab-m-top">
+                    <p className="small-title">I love cooking because...</p>
+                    <p className="italic-title">“Love, passion, my life, my everything”</p>
+                 </div>
+                 <div className="messages-text mt-5">
+                    <p className="small-title">I learned to cook at..</p>
+                    <p className="italic-title">“3* The Fat Duck,  2*Spondi Athens, 1* The Hinds Head, 1* he Pollen Street, Veneze Atens, Milos Athens”</p>
+                 </div>
+                 <div className="messages-text mt-5">
+                    <p className="small-title">A role model in the kitchen is</p>
+                    <p className="italic-title">“Fetan Adria, Juan Roca, Grand Archaz and many more”</p>
+                 </div>
+                 <div className="messages-text mt-5">
+                    <p className="small-title">A cooking secret..</p>
+                    <p className="italic-title">“Precision and passion for cooking”</p>
+                 </div>
                 </div>
-                <div className="container-fluid mt-5">
-                    <div className="row">
+            </section>
+
+            <section className="experience-slider mobile-p-0"> 
+                <div className="container ">
+                 <h2 className="font-black">Xenofon’s photo album</h2>
+                    <div className="row mt-5 mobile-m-0">
                         <div className="col-lg-2 col-md-6">
                             <div className="slider-img-plase">
-                                <img src="images/2.jpg" alt="2"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
+                                <img src="images/f-1.webp" alt="f-1"/> 
                             </div> 
                         </div>
                         <div className="col-lg-2 col-md-6">
                             <div className="slider-img-plase">
-                                <img src="images/3.jpg" alt="3"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
+                                <img src="images/f-2.webp" alt="f-2"/> 
                             </div> 
                         </div>
                         <div className="col-lg-2 col-md-6">
                             <div className="slider-img-plase">
-                            <img src="images/2.jpg" alt="2"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
+                                <img src="images/f-3.webp" alt="f-3"/> 
                             </div> 
                         </div>
                         <div className="col-lg-2 col-md-6">
                             <div className="slider-img-plase">
-                            <img src="images/3.jpg" alt="3"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
+                                <img src="images/f-4.webp" alt="f-4"/> 
                             </div> 
                         </div>
                         <div className="col-lg-2 col-md-6">
                             <div className="slider-img-plase">
-                            <img src="images/2.jpg" alt="2"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
+                                <img src="images/f-5.webp" alt="f-5"/> 
                             </div> 
                         </div>
                         <div className="col-lg-2 col-md-6">
                             <div className="slider-img-plase">
-                            <img src="images/3.jpg" alt="3"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
+                                <img src="images/f-1.webp" alt="f-1"/> 
                             </div> 
                         </div>
-                    </div> 
-                    <div className="text-center view-more mt-4"><a href="#">View More</a></div>
+                    </div>  
                 </div> 
             </section>
 
             <section className="services-part location-how mt-5 mobile-m-0">
                 <div className="container">
-                    <h2>Our customers said</h2> 
+                    <h2>Xenofon’s reviews</h2> 
                     <p className="dis-max-width mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin. Nascetur sapien sollicitudin eu consequat. Sem sed accumsan aliquet dapibus tincidunt lobortis sed mauris. </p>
                     <div className="row mt-5">
                       <div className="col-lg-4 col-md-6">
@@ -165,54 +182,7 @@ export default function Chefs() {
                       </div>                   
                 </div>
             </section>
-
-       
-            <section className="experience-slider mt-5">
-                <div className="container">
-                    <h3> Things to explore</h3>
-                </div>
-                <div className="container-fluid mt-5">
-                    <div className="row">
-                        <div className="col-lg-2 col-md-6">
-                            <div className="slider-img-plase">
-                                <img src="images/slider-1.webp" alt="slider-1"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
-                            </div> 
-                        </div>
-                        <div className="col-lg-2 col-md-6">
-                            <div className="slider-img-plase">
-                                <img src="images/slider-2.webp" alt="slider-2"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
-                            </div> 
-                        </div>
-                        <div className="col-lg-2 col-md-6">
-                            <div className="slider-img-plase">
-                                <img src="images/slider-3.webp" alt="slider-3"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
-                            </div> 
-                        </div>
-                        <div className="col-lg-2 col-md-6">
-                            <div className="slider-img-plase">
-                                <img src="images/slider-4.webp" alt="slider-4"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
-                            </div> 
-                        </div>
-                        <div className="col-lg-2 col-md-6">
-                            <div className="slider-img-plase">
-                                <img src="images/slider-2.webp" alt="slider-2"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
-                            </div> 
-                        </div>
-                        <div className="col-lg-2 col-md-6">
-                            <div className="slider-img-plase">
-                                <img src="images/slider-4.webp" alt="slider-4"/>
-                                <p className="plase-btn"><a href="#">Greece</a></p>
-                            </div> 
-                        </div>
-                    </div> 
-                    <div className="text-center view-more mt-4"><a href="#">View More</a></div>
-                </div> 
-            </section>
+  
 
             
 
---
+++ components/frontend/Contact.tsx
@@ -0,0 +1,149 @@
+import React, { useState ,useEffect} from 'react'
+export default function Contact() {
+    return(
+        <>
+            <section className="banner-part ">
+                <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-md-12">
+                    <img src="images/banner-6.jpg" alt="banner-6" className="w-100 border-0 "/>
+                    </div>
+                    <div className="col-lg-4 col-md-12">
+                    <div className="all-form">
+                      <h2>Contact us</h2>
+                      <label>Name Surname</label>
+                      <input type="text" placeholder="Name Surname" />
+                      <label>Email</label>
+                      <input type="text" placeholder="Email" />
+                      <label>Phone Number</label>
+                      <input type="text" placeholder="Phone Number" />
+                      <label>Your message</label>
+                      <textarea placeholder="Your message"></textarea>
+                      <div className="text-right">
+                      <button className="btn-send">Send</button>
+                      </div>
+                    </div>
+                    </div>
+                </div>    
+                </div>
+            </section>
+
+            <section className="faq-part">
+              <div className="container">
+                <h3>Frequently Asked Questions (FAQ)</h3>
+                    <ul className="table_header_button_section mt-3">
+                        <li><button className="table-btn">I am a Chef</button></li>
+                        <li><button className="table-btn btn-2">I am customer</button></li>
+                    </ul>
+                    <div className="accordion-part mt-5">
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="headingOne">
+                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                How does Private Chef Work?
+                                </button>
+                                </h2>
+                                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="headingTwo">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Who is the team behind Private Chefs? 
+                                </button>
+                                </h2>
+                                <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="headingThree">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                How can I contact Private Chefs? 
+                                </button>
+                                </h2>
+                                <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="heading4">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                                Do I need to pay in order to use Take a Chef? 
+                                </button>
+                                </h2>
+                                <div id="collapse4" className="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="heading5">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+                                Who can be a Chef? 
+                                </button>
+                                </h2>
+                                <div id="collapse5" className="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="heading6">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
+                                How can I receive requests through Take a Chef? 
+                                </button>
+                                </h2>
+                                <div id="collapse6" className="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="heading7">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse7" aria-expanded="false" aria-controls="collapse7">
+                                How can I receive requests through Take a Chef? 
+                                </button>
+                                </h2>
+                                <div id="collapse7" className="accordion-collapse collapse" aria-labelledby="heading7" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="heading8">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
+                                How can I receive requests through Take a Chef? 
+                                </button>
+                                </h2>
+                                <div id="collapse8" className="accordion-collapse collapse" aria-labelledby="heading8" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, you simply fill in the details of your event in an easy and dynamic way, and the best four local chefs interested will respond with a personalized menu and budget for you. Once you decide on a candidate based on their chef profile, menu proposal, and conversation, you can reserve the experience and the chef will take care of the rest. 
+                                </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+              </div>
+            </section>
+
+            
+           
+ 
+
+        </>
+    )
+}
