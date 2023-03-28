0463ca3abda0888cadad916b4f473815b45d3ea7
binarydata123 <binarydata.sale@gmail.com>
2023-03-28 17:41:38 +0530

    booking and popups pages updated

--- components/admin/Bookings/Bookings.tsx
+++ components/admin/Bookings/Bookings.tsx
@@ -160,7 +160,82 @@ export default function Bookings() {
 				</div>
 			</div>
 			<PopupModal show={modalConfirm} handleClose={modalConfirmClose}>
-				<h5>Booking #98283</h5>
+				<div className="popup-part new-modala">
+				  <h2 className="title-pop up-move">Booking #98283</h2>	
+				  <div className="offers">
+					<div className="row">
+						<div className="col-sm-6 col-12">
+							<div className="all-form"> 
+								<label className="f-w-4">Chef’s Offer:</label>
+								<input type="text" placeholder="432$" className="placeholder-goldan-text"/>
+							</div> 
+						</div>
+						<div className="col-sm-2  col-6"> 
+							<button className="pop-btn ">Menu 1</button> 
+						</div>
+						<div className="col-sm-2  col-6">							
+						 <button className="pop-btn">Menu 2</button>
+						</div>
+					</div>
+
+					<div className="row">
+						<div className="col-12">
+							<div className="all-form"> 
+								<label className="f-w-4">Chef’s Menu:</label>
+								<p className="f-10">Please pick one to send to the customer</p>
+							</div> 
+							<div className="pop-border-box">
+							<div className="faq-part">  
+								<div className="accordion-part ">
+								<div className="accordion" id="accordionExample">
+									<div className="accordion-item">
+										<h2 className="accordion-header" id="headingOne">
+										<button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+										How does Private Chef Work?
+										</button>
+										</h2>
+										<div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+										<div className="accordion-body">
+										Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest, 
+										</div>
+										</div>
+									</div>
+									<div className="accordion-item">
+										<h2 className="accordion-header" id="headingTwo">
+										<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+										Who is the team behind Private Chefs? 
+										</button>
+										</h2>
+										<div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+										<div className="accordion-body">
+										Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. As a guest...
+										</div>
+										</div>
+									</div>
+									<div className="accordion-item">
+										<h2 className="accordion-header" id="headingThree">
+										<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+										How can I contact Private Chefs? 
+										</button>
+										</h2>
+										<div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+										<div className="accordion-body">
+										Private Chefs allows you to enjoy the experience of having a private chef anywhere in the world. 
+										</div>
+										</div>
+									</div> 
+								</div>
+							</div> 
+							</div>
+							</div>
+							<div className="text-right">
+							 <div className="banner-btn"><a href="#">Start your journey</a></div>
+							</div>
+						</div> 
+					</div>
+
+				  </div>
+				</div>
 			</PopupModal>
         </>
     )
--- components/admin/Receipts/Receipts.tsx
+++ components/admin/Receipts/Receipts.tsx
@@ -1,5 +1,13 @@
 import React, { useState ,useEffect} from 'react'
+import PopupModal from '../../../components/commoncomponents/PopupModal';
 export default function Receipts() {
+	const [modalConfirm, setModalConfirm] = useState(false);
+	const modalConfirmOpen = () => {
+        setModalConfirm(true);
+    }
+    const modalConfirmClose = () => {
+        setModalConfirm(false);
+    }
     return (
         <>
 			<div className="table-part">
@@ -28,7 +36,7 @@ export default function Receipts() {
 								<td>Paypal</td>
 								<td>8/21/15</td>
 								<td>12/4/17</td>
-								<td><a href="#"><i className="fa-solid fa-ellipsis"></i></a></td>
+								<td><a href="#"  onClick={modalConfirmOpen}><i className="fa-solid fa-ellipsis"></i></a></td>
 							</tr>
 							<tr>
 								<td>2493</td>
@@ -134,6 +142,215 @@ export default function Receipts() {
 					</table>
 				</div>
 			</div>
+ 
+			<PopupModal show={modalConfirm} handleClose={modalConfirmClose}>
+				<h5 className="title-pop up-move">Booking #98283</h5> 
+                    <ul className="nav nav-pills mt-4 mb-4 popup-align-left" id="pills-tab" role="tablist">
+                        <li className="nav-item" role="presentation">
+                            <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Photos</button>
+                        </li>
+                        <li className="nav-item" role="presentation">
+                            <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Receipts</button>
+                        </li>
+                      <li className="add-plus-li"><a href="#"  className="add-plus"><i className="fa-solid fa-plus"></i> Add new</a></li>
+                    </ul>
+                    <div className="tab-content" id="pills-tabContent">
+                        <div className="tab-pane fade show active" id="pills-home" role="tabpanel"  aria-labelledby="pills-home-tab">
+						<div className="row mt-4"> 
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+								<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" />  
+							</div>
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							
+						</div>
+						<div className="row mt-4"> 
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+								<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" />  
+							</div>
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							
+						</div>
+						<div className="row mt-4"> 
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+								<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" />  
+							</div>
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							
+						</div>
+						<div className="row mt-4"> 
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+								<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" />  
+							</div>
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							<div className="col-sm-4"> 
+							<div className="slider-img-plase"> 
+							<img src={process.env.NEXT_PUBLIC_BASE_URL+'images/food-pop-img.jpg'} alt="food-pop-img" /> 
+							</div> 
+							</div>
+							
+						</div>
+                        </div>
+                        <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+						<div className="table-box">
+					<table className="table table-borderless">
+						<thead>
+							<tr>
+								<th scope="col">Field 1</th>
+								<th scope="col">Field 2</th>
+								<th scope="col">Field 3</th>
+								<th scope="col">Field 4</th>
+								<th scope="col">Field 5</th> 
+							</tr>
+						</thead>
+						<tbody>
+						    <tr>
+								<td><input type="text" placeholder="10/11/21" className="placeholder-goldan-text width-100"/></td>
+								<td><input type="text" placeholder="10/11/21" className="placeholder-goldan-text width-100" /></td>
+								<td><input type="text" placeholder="10/11/21" className="placeholder-goldan-text width-100" /></td>
+								<td><input type="text" placeholder="10/11/21" className="placeholder-goldan-text width-100" /></td>
+								 <td>
+									<div className="row">
+									 <div className="col-9">
+										<input type="text" placeholder="10/11/21" className="placeholder-goldan-text width-100" />
+								     </div>
+										<div className="col-3 pl-0 pr-0 icon-des">
+										 <i className="fa-solid fa-check"></i> 
+										 <i className="fa-solid fa-xmark"></i>
+										</div> 
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+							<tr>
+								<td>2493</td>
+								<td>5122389082</td>
+								<td>$539.2</td>
+								<td>$539.2</td>
+								<td>XXXX XXXX XXXX 2459</td> 
+							</tr>
+
+						 
+						</tbody>
+					</table>
+				</div>	
+                        </div>
+                      
+                    </div> 
+ 
+
+				
+
+
+				 
+			</PopupModal>
         </>
     )
 }
