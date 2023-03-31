51495e093f3c156139fa4d0e70fb6ff041bb3f83
binarydata123 <binarydata.sale@gmail.com>
2023-03-31 18:20:25 +0530

    notification updates

--- components/chef/Notification/Notification.tsx
+++ components/chef/Notification/Notification.tsx
@@ -6,7 +6,104 @@ export default function Notification() {
 				<h2>Notification</h2>
 				  
 				<div className="notification-box">
-				  
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-arrow-up-wide-short"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-money-check-dollar"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-dollar-sign"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-file-invoice-dollar"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-circle"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-circle"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-circle"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-circle"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
+
+					<div className="notification-hover">
+					<div className="d-flex">
+						<div className="circle">
+						<i className="fa-solid fa-circle"></i>
+						</div>
+						<div className="text-noti">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie laoreet eget penatibus cum lectus. Accumsan, in odio bibendum praesent sollicitudin.</p>
+						</div>
+					</div>
+					</div>
 				 </div>
 				 
 			</div>
