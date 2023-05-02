99fc74d23ee911d017c07d13de63b6aa93b1f90f
Binarydata12 <binarydata.vipin@gmail.com>
2023-05-02 09:12:19 +0530

    working on filter chef page

--- components/admin/Chefs/Chefs.tsx
+++ components/admin/Chefs/Chefs.tsx
@@ -44,60 +44,234 @@ export default function Chefs() {
 		
 
     return (
-        <>
-			<div className="table-part">
-				<h2>Chefs</h2>
-				<ul className="table_header_button_section p-r">
-					<li><button className="table-btn">Total</button></li>
-					<li className="right-li"><button className="table-btn border-radius round-white">Filter </button></li> 
-					</ul> 
+      <>
+        <div className="table-part">
+          <h2>Chefs</h2>
+          <ul className="table_header_button_section p-r">
+            <li>
+              <button className="table-btn">Total</button>
+            </li>
+            <li className="right-li">
+              <button
+                className="table-btn border-radius round-white"
+                onClick={() => setModalConfirm(true)}
+              >
+                Filter{" "}
+              </button>
+            </li>
+          </ul>
 
-				
-				<div className="table-box">
-					<table className="table table-borderless">
-						<thead>
-							<tr>
-								<th scope="col">Photo</th>
-								<th scope="col">Name/Surname</th>
-								<th scope="col">Current Location</th>
-								<th scope="col">Cuisines</th>
-								<th scope="col">Location</th>
-								<th scope="col">Dietary restrictios</th>
-								<th scope="col">Rating</th>
-								<th scope="col"></th>
-							</tr>
-						</thead>
-						<tbody>
-						{chefs.map(chef => (
-						<tr key={chef.id}>
-							{chef.pic ? (
-                      <td className='chefs_pic'><img src={process.env.NEXT_PUBLIC_IMAGE_URL+'images/chef/users/' + chef.pic } alt=""/></td>
-							) : ( 
-					 <td className='chefs_pic'><img src={process.env.NEXT_PUBLIC_IMAGE_URL+'/images/placeholder.jpg'} alt=""/></td>
-								)}
-                      <td>{ chef.name+ " " +chef.surname }</td>
-                      <td>{ chef.address}</td>
-                      <td>
+          <div className="table-box">
+            <table className="table table-borderless">
+              <thead>
+                <tr>
+                  <th scope="col">Photo</th>
+                  <th scope="col">Name/Surname</th>
+                  <th scope="col">Current Location</th>
+                  <th scope="col">Cuisines</th>
+                  <th scope="col">Location</th>
+                  <th scope="col">Dietary restrictios</th>
+                  <th scope="col">Rating</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {chefs.map((chef) => (
+                  <tr key={chef.id}>
+                    {chef.pic ? (
+                      <td className="chefs_pic">
+                        <img
+                          src={
+                            process.env.NEXT_PUBLIC_IMAGE_URL +
+                            "images/chef/users/" +
+                            chef.pic
+                          }
+                          alt=""
+                        />
+                      </td>
+                    ) : (
+                      <td className="chefs_pic">
+                        <img
+                          src={
+                            process.env.NEXT_PUBLIC_IMAGE_URL +
+                            "/images/placeholder.jpg"
+                          }
+                          alt=""
+                        />
+                      </td>
+                    )}
+                    <td>{chef.name + " " + chef.surname}</td>
+                    <td>{chef.address}</td>
+                    <td>
                       <ul>
-                     <li>{chef.cuisine_name}</li>
-                     {/* <li>Italian</li>
+                        <li>{chef.cuisine_name}</li>
+                        {/* <li>Italian</li>
                      <li>+4</li> */}
-                     </ul>
-                     </td>
-                     <td>Ut pulvinar.</td>
-                     <td>Arcu nibh non.</td>
-                     <td>Eu nibh.</td>
-                     <td><a href="#"><i className="fa-solid fa-ellipsis"></i></a></td>
-                    </tr>
-                    ))}
-
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<PopupModal show={modalConfirm} handleClose={modalConfirmClose}>
-				
-			</PopupModal>
-        </>
-    )
+                      </ul>
+                    </td>
+                    <td>Ut pulvinar.</td>
+                    <td>Arcu nibh non.</td>
+                    <td>Eu nibh.</td>
+                    <td>
+                      <a href="#">
+                        <i className="fa-solid fa-ellipsis"></i>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <PopupModal show={modalConfirm} handleClose={modalConfirmClose}>
+          <div className="accordion" id="accordionExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  Cuisines
+                </button>
+              </h2>
+              <div
+                id="collapseOne"
+                className="accordion-collapse collapse show"
+                aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+				<div className='container chkbox'>
+                  <div className='row'>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Local Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Spanish Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Local Cuisine</label>
+					</div>
+					</div>
+					<div className='row'>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Greek Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Japanese Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Local Cuisine</label>
+					</div>
+					</div>
+					<div className='row'>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Thai Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Mexican Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Local Cuisine</label>
+					</div>
+					</div>
+					<div className='row'>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Chinese Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Italian Cuisine</label>
+					</div>
+					<div className='col-sm-4'>
+						<input type="checkbox" />
+						<label>Local Cuisine</label>
+					</div>
+					</div>
+				  </div>
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingTwo">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  Locations
+                </button>
+              </h2>
+              <div
+                id="collapseTwo"
+                className="accordion-collapse collapse"
+                aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classNamees that we use to style each element. These
+                  classNamees control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingThree">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseThree"
+                  aria-expanded="false"
+                  aria-controls="collapseThree"
+                >
+                  Dietaty Restrictions
+                </button>
+              </h2>
+              <div
+                id="collapseThree"
+                className="accordion-collapse collapse"
+                aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classNamees that we use to style each element. These
+                  classNamees control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the{" "}
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+            </div>
+          </div>
+        </PopupModal>
+      </>
+    );
 }
--- components/admin/layouts/sidebar.tsx
+++ components/admin/layouts/sidebar.tsx
@@ -31,7 +31,7 @@ export default function Sidebar(): JSX.Element {
                     <div className="row">
                         <div className="col-lg-3 col-md-4 col-4 pr-0"> 
                             <a href="/">
-                            {currentUserData.pic == 'null' ? <img src={process.env.NEXT_PUBLIC_IMAGE_URL+'/images/users.jpg'} alt="user-menu"/> : <img src={process.env.NEXT_PUBLIC_IMAGE_URL+'images/chef/users/'+currentUserData.pic} alt="user-menu"/>} 
+                            {currentUserData.pic == 'null' ? <img src={process.env.NEXT_PUBLIC_IMAGE_URL+'/images/users.jpg'} alt="user-menu"/> : <img src={process.env.NEXT_PUBLIC_IMAGE_URL+'/images/chef/users/'+currentUserData.pic} alt="user-menu"/>} 
                                 </a>
                         </div>
                         <div className="col-lg-9 col-md-8 col-8">
