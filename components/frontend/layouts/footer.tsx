e78e710f14f235a3797ae1e2476bc46d22d2869a
binarydataJagmohan <info.binarydata@gmail.com>
2023-11-23 17:59:32 +0530

    code new  add table width size same here

--- components/frontend/layouts/footer.tsx
+++ components/frontend/layouts/footer.tsx
@@ -4,6 +4,7 @@ import ChefLocation from '../../../components/frontend/FooterLocation/ChefLocati
 import { subscribe } from "../../../lib/frontendapi"
 import swal from "sweetalert";
 import { ToastContainer, toast } from "react-toastify";
+import Link from 'next/link';
 export default function Footer() {
 
   interface Location {
@@ -92,29 +93,29 @@ export default function Footer() {
         <div className="container-fluid">
           <div className="footer-part ft-top-border text-center">
             <h6 className="color-bs8639 ">Subscribe to our newsletter</h6>
-            <form className="email-part mt-2" onSubmit={handlesubmit}>
+            <form className="email-part mt-2 mb-4" onSubmit={handlesubmit}>
               <input type="email" placeholder="Enter your email" value={email || ''} onChange={handleEmailChange} required/>
               <button type="submit" className="btn-send w-10 mx-3 mt-3" style={{padding:'6px 14px'}}>Submit</button>
             </form>
           
             < ChefLocation />
             <ul className="ft-nav mt-3">
-              <li><a href="/contact/#faq">FAQ</a></li>
-              <li><a href="/contact">Contact Us</a></li>
-              <li><a href="/whoweare">Who we are</a></li>
-              <li><a href="/ourchefs">Our Chefs</a></li>
-              <li><a href="/ourservices">Our Services</a></li>
-              <li><a href="/ourlocations">Locations</a></li>
-              <li><a href="/privacypolicy">Privacy Policy</a></li>
-              <li><a href="/termsconditions">Terms & Conditions</a></li>
-              <li><a href="/cookies">Cookies</a></li>
+              <li><Link href="/contact/#faq">FAQ</Link></li>
+              <li><Link href="/contact">Contact Us</Link></li>
+              <li><Link href="/whoweare">Who we are</Link></li>
+              <li><Link href="/ourchefs">Our Chefs</Link></li>
+              <li><Link href="/ourservices">Our Services</Link></li>
+              <li><Link href="/ourlocations">Locations</Link></li>
+              <li><Link href="/privacypolicy">Privacy Policy</Link></li>
+              <li><Link href="/termsconditions">Terms & Conditions</Link></li>
+              <li><Link href="/cookies">Cookies</Link></li>
 
             </ul>
-            <ul className="ft-social mt-3">
-              <li><a href="https://www.facebook.com/" target="_blank"><i className="fa-brands fa-facebook"></i></a></li>
-              <li><a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-instagram"></i></a></li>
-              <li><a href="https://www.linkedin.com/" target="
-              "><i className="fa-brands fa-linkedin"></i></a></li>
+             <ul className="ft-social mt-3">
+              <li><Link href="https://www.facebook.com/" target="_blank"><i className="fa-brands fa-facebook"></i></Link></li>
+              <li><Link href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-instagram"></i></Link></li>
+              <li><Link href="https://www.linkedin.com/" target="
+              "><i className="fa-brands fa-linkedin"></i></Link></li>
             </ul>
             <p className="font-small color-bs8639 mt-3 mb-0">Copyright © 2023 Private Chefs All rights reserved</p>
           </div>
